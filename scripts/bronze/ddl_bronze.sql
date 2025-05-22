255e7489326e8be21f86b4ef1e2906bf3b24a577
Arup Patra <aruppatra2004@gmail.com>
2025-05-22 15:29:30 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -11,12 +11,12 @@ IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
 GO
 -- create "bronze.crm_cust_info" table
 CREATE TABLE bronze.crm_cust_info (
-	cst_id				INT,
-	cst_key				NVARCHAR(50),
+	cst_id		    	INT,
+	cst_key			NVARCHAR(50),
 	cst_firstname		NVARCHAR(50),
-	cst_lastname		NVARCHAR(50),
-	cst_material_status NVARCHAR(50),
-	cst_gndr			NVARCHAR(50),
+	cst_lastname	    	NVARCHAR(50),
+	cst_material_status 	NVARCHAR(50),
+	cst_gndr		NVARCHAR(50),
 	cst_create_date		DATE
 );
 GO
@@ -31,8 +31,8 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_nm		 NVARCHAR(50),
 	prd_cost	 INT,
 	prd_line	 NVARCHAR(50),
-	prd_start_dt DATETIME,
-	prd_end_dt   DATETIME
+	prd_start_dt	 DATETIME,
+	prd_end_dt   	 DATETIME
 );
 GO
 -- if "bronze.crm_sales_details" table exit the drop
@@ -41,15 +41,15 @@ IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
 GO
 -- create "bronze.crm_sales_details" table
 CREATE TABLE bronze.crm_sales_details (
-	sls_ord_num  NVARCHAR(50),
-	sls_prd_key  NVARCHAR(50),
-	sls_cust_id	 INT,
-	sls_order_dt INT,
-	sls_ship_dt  INT,
-	sls_due_dt   INT,
-	sls_sales    INT,
-	sls_quantity INT,
-	sls_price    INT
+	sls_ord_num  	NVARCHAR(50),
+	sls_prd_key  	NVARCHAR(50),
+	sls_cust_id	INT,
+	sls_order_dt 	INT,
+	sls_ship_dt 	INT,
+	sls_due_dt   	INT,
+	sls_sales    	INT,
+	sls_quantity	INT,
+	sls_price    	INT
 );
 GO
 -- if "bronze.erp_loc_a101" table exit then drop
@@ -79,8 +79,8 @@ IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 GO
 -- create "bronze.erp_px_cat_g1v2" table
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-	cid			NVARCHAR(50),
-	cat			NVARCHAR(50),
+	cid		NVARCHAR(50),
+	cat		NVARCHAR(50),
 	subcat		NVARCHAR(50),
-	maintenance NVARCHAR(50)
+	maintenance 	NVARCHAR(50)
 );
